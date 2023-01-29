190209fe0a048864dab9868b1fe144eb431b8ad2
mgreen <mgreen@bournemouth.ac.uk>
2023-01-29 19:05:30 +0000

    improved reading data added a progress bar

--- 001-00-e1_read_data_in.R
+++ 001-00-e1_read_data_in.R
@@ -1,7 +1,9 @@
+be_verbose = T # TRUE for verbose logging
+
 library(reticulate)
-library(tidyverse)
 library(tictoc)
 library(crayon)
+suppressPackageStartupMessages(library(tidyverse))
 
 tic()
 
@@ -11,9 +13,28 @@ if (Sys.info()[['sysname']]=="Windows"){
 
 e1allsubs = tibble()
 indir = "e1_pickles"
-numberoftrials = 20
 
-for (infilename in list.files(indir, pattern=".pickle$")) {
+numberoftrials = 20
+files_in = list.files(indir, pattern=".pickle$")
+n_total = length(files_in)
+n_complete = 0
+
+for (infilename in files_in) {
+  
+  message(
+    str_c(
+      "\r",
+      "Reading files: ",
+      "[",
+      rep("+", n_complete) %>% str_flatten(),
+      rep(bold("o"), times = (n_complete != n_total)), # once if there are any remaining trials not completed
+      rep("-", n_total - n_complete - (n_complete != n_total)) %>% str_flatten(),
+      "]"
+    ) %>% blue(),
+    appendLF = F
+  )
+  
+  if(be_verbose){message("\n", blue(substr(infilename, 1, 4)))}
   
   if (Sys.info()[['sysname']]=="Windows"){
     dos2unix(
@@ -25,6 +46,8 @@ for (infilename in list.files(indir, pattern=".pickle$")) {
   } else {
     infilecontent <- py_load_object(file.path(indir, infilename))
   }
+  
+  first_block = ifelse(grepl("cluster", infilecontent[[1]][['trial_name']]), "patchy", "dispersed")
 
   for (trialnumber in 1:numberoftrials) {
     trialnumber <- as.character(trialnumber)
@@ -41,6 +64,7 @@ for (infilename in list.files(indir, pattern=".pickle$")) {
       outf[i, 'rr'] = ifelse(grepl("cluster", content[['trial_name']]), "patchy", "dispersed")
       outf[i, 'tb'] = content[['trial_num_in_block']]
       outf[i, 'te'] = paste(ifelse(outf[i, 'rr']=="dispersed", 'd', 'p'), str_pad(outf[i, 'tb'],2,'left',"0"),sep=":")
+      outf[i, 'tt'] = as.numeric(NA)
       outf[i, "st"] = ifelse(content[['trial_num_in_block']] <=5, "early", "late")
       outf[i, 'ix'] = content[['samples']][[samplenumber]]$sample_index
       outf[i, 'tm'] = content[['samples']][[samplenumber]]$sample_timestamp
@@ -52,26 +76,42 @@ for (infilename in list.files(indir, pattern=".pickle$")) {
       outf[i, 'fr'] = content[['samples']][[samplenumber]]$fruit_tally
     } # end of loop through samples
     
-    m = paste("subject", substr(infilename, 1, 4), "trial", str_pad(trialnumber,2,"left"," "), "size", str_pad(nrow(outf),3,"left"," "))
-    message(blue(m), appendLF = FALSE)
+    if(be_verbose){
+      message(blue(substr(infilename, 1, 4), "trial", str_pad(trialnumber,2,"left"," "), "size", str_pad(nrow(outf),3,"left"," "), " "), appendLF = F)
+    }
     
-    message(blue("\tcorrecting row order and making index strictly sequential"))
+    # correcting row order and 
+    # making index strictly sequential
+    if(be_verbose){message(blue("putting in row order by time stamp; "), appendLF = F)}
+    if(be_verbose){message(blue("correcting skips in index"), appendLF = T)}
     outf <- outf %>% 
       arrange(tm) %>%              # correct for bad row-order 
       mutate(ix = seq_along(tm))   # correct for where ix skips (i.e., where it has non-strictly-incrementing-by-one order)
     
+    # implement a trial_in_experiment number
+    # that is sensitive to the order in which they did the blocks
+    outf <- outf %>% 
+      mutate(tt = case_when(
+        rr==first_block~(100+tb),
+        rr!=first_block~(200+tb),
+      ))
+
     # add this participant to the allsubs tibble
     e1allsubs <- bind_rows(e1allsubs, outf)
 
   } # end of loop through trials
   
   # this participant is finished - go back for the next one after adding to allsubs
-  message(green(paste(substr(infilename, 1, 4), "done")))
+  n_complete = n_complete + 1
 }
 
 # all participants are done
-message(green("all subjects done"))
+message("")
+message(blue(n_complete, "of", n_total, "subjects done\n"))
+
 #save out
 saveRDS(e1allsubs, "001-00-e1-data.RDS")
 
 source("001-01-e1_sanity_check_raw_data.R")
+
+toc()
